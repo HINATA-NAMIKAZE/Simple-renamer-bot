715dc7f856b342a9b96178989ffef5668a8e783a
HINATA-NAMIKAZE <157386983+HINATA-NAMIKAZE@users.noreply.github.com>
2024-07-09 21:00:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "4857766"))
+API_HASH = environ.get("API_HASH", "6c3c6facf5598a4b318e138f8c407028")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5950478723:AAELUayqnKQdVZoio9PuVXskEnfdfXInpOg")
+ADMIN = int(environ.get("ADMIN", "1596559467"))          
 CAPTION = environ.get("CAPTION", "")
 DOWNLOAD_LOCATION = "./DOWNLOADS"
-API = environ.get("API", "") # shortlink api
-URL = environ.get("URL", "") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+API = environ.get("API", "ccdd7d73458653f07aecbaa1d8159457965391b4") # shortlink api
+URL = environ.get("URL", "publicearn.com") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/maheshsirop") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "rashmikarenamerbot") # bot username without @
 VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
